aefd58b35adca5e709ad8d70d31d591022ad9bd6
Brad Schick <schickb@gmail.com>
2023-05-20 10:47:13 -0700

    moved tests to subdirectory

--- tests/test_hylat.py
+++ tests/test_hylat.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. """
 
-import hylat
+from context import hylat
 import pytest
 import json
 import numpy as np
